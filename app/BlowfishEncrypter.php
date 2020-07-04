6192dbd2cf00d25c2907f0d4aab059268083c2e6
kyohei-tsuno <k.tsuno@ichain.co.jp>
2020-07-04 23:18:01 +0900

    install phpseclib

---
+++ app/BlowfishEncrypter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use phpseclib\Crypt\Blowfish;
+use Illuminate\Contracts\Encryption\Encrypter as EncrypterContract;
+
+class BlowfishEncrypter implements EncrypterContract
+{
+    protected $encrypter;
+
+    public function __construct()
+    {
+        $this->encrypter = new Blowfish();
+        $this->encrypter->setKey($key);
+    }
+
+    public function encrypt($value, $serialize = true)
+    {
+        // TODO: Implement encrypt() method.
+        return $this->encrypter->encrypt($value);
+    }
+
+    public function decrypt($payload, $unserialize = true)
+    {
+        // TODO: Implement decrypt() method.
+        return $this->encrypter->decrypt($payload);
+    }
+
+}
